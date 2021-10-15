9df2387e4039fe3c87f5332ef637fd0997e41424
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-10-15 17:17:25 +0100

    debugging

--- src/UrlRegistry.php
+++ src/UrlRegistry.php
@@ -118,7 +118,7 @@ class UrlRegistry
       {
         $deploy = null !== env('MODULLO_DEPLOY_MODE') ? env('MODULLO_DEPLOY_MODE') : 'default';
         if ($deploy == "lambda") {
-            return 'v1'; //'staging/v1'
+            return 'staging/v1'; //'staging/v1'
         } else {
             return 'v1';
         }
