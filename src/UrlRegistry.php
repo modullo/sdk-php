a0721fc8a12c5b95ab4ae761b812e249ff10ea15
Damilare Abiodun <dami@20ksites.com>
2022-06-21 18:45:30 +0100

    Troubleshooting lmshub test 3

--- src/UrlRegistry.php
+++ src/UrlRegistry.php
@@ -27,7 +27,7 @@ class UrlRegistry
     {
         $enviroments = include("config/environments.php");
         $envs = array_keys($enviroments);
-        dd($env);
+        dd($envs);
         # get the available environment
         $this->environment = !in_array(strtolower($env), $envs, true) ? 'staging' : strtolower($env);
         $base = $enviroments[$this->environment];
